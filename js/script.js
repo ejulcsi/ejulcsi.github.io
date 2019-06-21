8d4956ffdffb5a6bd7a199916802a7b30aad776b
julcsi <ejulcsi@gmail.com>
2019-06-21 21:44:47 +0200

    redesign

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-$('.text').hide();
+const links = document.querySelectorAll('nav a')
 
-$('.circle_a').click(function(){
-    $(this).next().slideToggle();
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = e.target.getAttribute('href').slice(1)
+    const section = document.getElementById(id)
+
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    })
+  })
+})
+
+window.addEventListener('scroll', () => {
+  links.forEach(i => {
+    const id = i.getAttribute('href').slice(1)
+    const section = document.getElementById(id)
+
+    if (window.pageYOffset >= section.offsetTop - (section.offsetHeight/2) && window.pageYOffset < section.offsetTop + (section.offsetHeight/2)) {
+      i.classList.add('active')
+    } else {
+      i.classList.remove('active')
+    }
+  })
 })
